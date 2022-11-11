b49e92a7d180d38bbaa4e56613ec68315a3ebaa7
Anar Musayev <anarmusayev@digirella.az>
2022-11-11 12:51:41 +0400

    change criteria api implementation

--- src/main/java/com/google/querydsl/repository/CustomerQueryDsl.java
+++ src/main/java/com/google/querydsl/repository/CustomerQueryDsl.java
@@ -1,10 +1,9 @@
 package com.google.querydsl.repository;
 
 import com.google.querydsl.entity.Customer;
-import com.google.querydsl.service.FilterParams;
 import com.querydsl.core.types.Predicate;
 import java.util.List;
 
 public interface CustomerQueryDsl {
-    List<Customer> findAllQueryDsl(Predicate predicate);
+    List<Customer> getAllQueryDsl(Predicate predicate);
 }
